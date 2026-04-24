1bd5b56d276bbfb6e38b05f5604ddbb2ace6b822
Nickinfinity <nick.dexsys@gmail.com>
2026-04-24 17:39:23 -0500

    Config window: initial config

--- src/extension.ts
+++ src/extension.ts
@@ -13,11 +13,19 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('obsidian-notes-and-snippets.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('obsidian-notes-and-snippets.config', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from Obsidian Notes &amp; Snippets!');
+    let panel = vscode.window.createWebviewPanel(
+    "config",
+    "AI Obsidian Snippets & Tools - CONFIG",
+    vscode.ViewColumn.One,
+    {}
+    );
+    //panel.webview.html = getWebviewContent();
 	});
+
 
 	context.subscriptions.push(disposable);
 }
